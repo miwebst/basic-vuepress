64cdf8dff5d71c19f231f6fdbc64442d7dc8ee11
Jen Looper <jen.looper@gmail.com>
2019-11-23 14:17:32 -0500

    bare bones VuePress!

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'A very basic VuePress site',
+	description: 'Site Description',
+	themeConfig: {
+		search: true,
+		lastUpdated: 'Last Updated',
+		sidebar: [
+			'/articles/1',
+			'/articles/2'
+		]           
+	}
+};
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+---
+home: true
+actionText: Let's Start!
+actionLink: /articles/1
+---
